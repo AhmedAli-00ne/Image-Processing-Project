cc6652126e976687a138b4d30e050ec6ff5ea8f6
AhmedAli-00ne <84929946+AhmedAli-00ne@users.noreply.github.com>
2023-08-13 01:46:54 +0300

    added more filters

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -38,6 +38,8 @@
             this.HistoEqBut = new System.Windows.Forms.Button();
             this.InterpBut = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -79,6 +81,7 @@
             this.AdaptiveFBut.TabIndex = 4;
             this.AdaptiveFBut.Text = "Adaptive Filter";
             this.AdaptiveFBut.UseVisualStyleBackColor = true;
+            this.AdaptiveFBut.Click += new System.EventHandler(this.AdaptiveFBut_Click);
             // 
             // AvergFBut
             // 
@@ -88,6 +91,7 @@
             this.AvergFBut.TabIndex = 5;
             this.AvergFBut.Text = "Averaging Filter";
             this.AvergFBut.UseVisualStyleBackColor = true;
+            this.AvergFBut.Click += new System.EventHandler(this.AvergFBut_Click);
             // 
             // GaussFBut
             // 
@@ -135,11 +139,33 @@
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 380);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 58);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(176, 379);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(101, 59);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.InterpBut);
             this.Controls.Add(this.HistoEqBut);
@@ -168,6 +194,8 @@
         private System.Windows.Forms.Button HistoEqBut;
         private System.Windows.Forms.Button InterpBut;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OpenCvSharp;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
 
 namespace Image_Project
 {
     public partial class Form1 : Form
     {
         Bitmap OriginalImage;
+        Mat CVImage;
         bool ImageLoaded = false;
         bool MenuToggle = false;
+        bool AdaptiveButtons = false;
         public Form1()
         {
             InitializeComponent();
@@ -25,17 +29,22 @@ namespace Image_Project
             InputMask.Hide();
             HistoEqBut.Hide();
             InterpBut.Hide();
+            button2.Hide();
+            button3.Hide();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            button2.Hide();
+            button3.Hide();
             OpenFileDialog open = new OpenFileDialog();
-            open.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
+            open.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp; *.png)|*.jpg; *.jpeg; *.gif; *.bmp; *.png";
             if (open.ShowDialog() == DialogResult.OK)
             {
                 pictureBox1.Image = new Bitmap(open.FileName);
                 pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
                 OriginalImage = new Bitmap(open.FileName);
+                CVImage = Cv2.ImRead(open.FileName, ImreadModes.Grayscale);
                 ImageLoaded = true;
             }
         }
@@ -47,15 +56,41 @@ namespace Image_Project
 
         private void button3_Click(object sender, EventArgs e)
         {
-            //apply median filter to the original image without using Filters method
-            
-            
+            button2.Hide();
+            button3.Hide();
+            if (ImageLoaded)
+            {
+                Mat newImage = new Mat(CVImage.Width, CVImage.Height, 0);
+                for (int i = 0; i < newImage.Width; i++)
+                {
+                    for (int j = 0; j < newImage.Height; j++)
+                    {
+                        List<int> neighboors = new List<int>();
+                        neighboors.Add(CVImage.At<byte>(i, j));
+                        neighboors.Add(CVImage.At<byte>(i - 1, j - 1));
+                        neighboors.Add(CVImage.At<byte>(i - 1, j));
+                        neighboors.Add(CVImage.At<byte>(i - 1, j + 1));
+                        neighboors.Add(CVImage.At<byte>(i, j - 1));
+                        neighboors.Add(CVImage.At<byte>(i, j + 1));
+                        neighboors.Add(CVImage.At<byte>(i + 1, j - 1));
+                        neighboors.Add(CVImage.At<byte>(i + 1, j));
+                        neighboors.Add(CVImage.At<byte>(i + 1, j + 1));
+                        neighboors.Sort();
+                        newImage.Set<byte>(i, j, (byte)neighboors[4]);
+                    }
+                }
+                Cv2.ImShow("Median Filter", newImage);
+            }
+            else
+            {
+                MessageBox.Show("Load the Image First");
+            }
         }
 
         private void MenuBut_Click(object sender, EventArgs e)
         {
             MenuToggle = !MenuToggle;
-            if(MenuToggle)
+            if (MenuToggle)
             {
                 MedianFBut.Show();
                 AdaptiveFBut.Show();
@@ -74,7 +109,121 @@ namespace Image_Project
                 InputMask.Hide();
                 HistoEqBut.Hide();
                 InterpBut.Hide();
+                button2.Hide();
+                button3.Hide();
+            }
+        }
+
+        private void AdaptiveFBut_Click(object sender, EventArgs e)
+        {
+
+            if(ImageLoaded)
+            {
+                button2.Text = "Max";
+                button3.Text = "Min";
+                button2.Show();
+                button3.Show();
+                MessageBox.Show("Choose the Type of Adaptive Filter");
+                AdaptiveButtons = true;
+            }    
+        }
+
+        private void AvergFBut_Click(object sender, EventArgs e)
+        {
+            button2.Hide();
+            button3.Hide();
+            if (ImageLoaded)
+            {
+                Mat newImage = new Mat(CVImage.Width, CVImage.Height, 0);
+                for (int i = 0; i < newImage.Width; i++)
+                {
+                    for (int j = 0; j < newImage.Height; j++)
+                    {
+                        List<int> neighboors = new List<int>();
+                        neighboors.Add(CVImage.At<byte>(i, j));
+                        neighboors.Add(CVImage.At<byte>(i - 1, j - 1));
+                        neighboors.Add(CVImage.At<byte>(i - 1, j));
+                        neighboors.Add(CVImage.At<byte>(i - 1, j + 1));
+                        neighboors.Add(CVImage.At<byte>(i, j - 1));
+                        neighboors.Add(CVImage.At<byte>(i, j + 1));
+                        neighboors.Add(CVImage.At<byte>(i + 1, j - 1));
+                        neighboors.Add(CVImage.At<byte>(i + 1, j));
+                        neighboors.Add(CVImage.At<byte>(i + 1, j + 1));
+                        int sum = 0;
+                        foreach (int n in neighboors)
+                        {
+                            sum += n;
+                        }
+                        newImage.Set<byte>(i, j, (byte)(sum / 9));
+                    }
+                }
+                Cv2.ImShow("Average Filter", newImage);
+            }
+            else
+            {
+                MessageBox.Show("Load the Image First");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Hide();
+            button3.Hide();
+            if (AdaptiveButtons)
+            {
+                Mat newImage = new Mat(CVImage.Width, CVImage.Height, 0);
+                for (int i = 0; i < newImage.Width; i++)
+                {
+                    for (int j = 0; j < newImage.Height; j++)
+                    {
+                        List<int> neighboors = new List<int>();
+                        neighboors.Add(CVImage.At<byte>(i, j));
+                        neighboors.Add(CVImage.At<byte>(i - 1, j - 1));
+                        neighboors.Add(CVImage.At<byte>(i - 1, j));
+                        neighboors.Add(CVImage.At<byte>(i - 1, j + 1));
+                        neighboors.Add(CVImage.At<byte>(i, j - 1));
+                        neighboors.Add(CVImage.At<byte>(i, j + 1));
+                        neighboors.Add(CVImage.At<byte>(i + 1, j - 1));
+                        neighboors.Add(CVImage.At<byte>(i + 1, j));
+                        neighboors.Add(CVImage.At<byte>(i + 1, j + 1));
+                        neighboors.Sort();
+                        newImage.Set<byte>(i, j, (byte)neighboors[8]);
+                    }
+                }
+                Cv2.ImShow("Adaptive Filter (Max)", newImage);
+                AdaptiveButtons = false;
+            }
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            button2.Hide();
+            button3.Hide();
+            if (AdaptiveButtons)
+            {
+                Mat newImage = new Mat(CVImage.Width, CVImage.Height, 0);
+                for (int i = 0; i < newImage.Width; i++)
+                {
+                    for (int j = 0; j < newImage.Height; j++)
+                    {
+                        List<int> neighboors = new List<int>();
+                        neighboors.Add(CVImage.At<byte>(i, j));
+                        neighboors.Add(CVImage.At<byte>(i - 1, j - 1));
+                        neighboors.Add(CVImage.At<byte>(i - 1, j));
+                        neighboors.Add(CVImage.At<byte>(i - 1, j + 1));
+                        neighboors.Add(CVImage.At<byte>(i, j - 1));
+                        neighboors.Add(CVImage.At<byte>(i, j + 1));
+                        neighboors.Add(CVImage.At<byte>(i + 1, j - 1));
+                        neighboors.Add(CVImage.At<byte>(i + 1, j));
+                        neighboors.Add(CVImage.At<byte>(i + 1, j + 1));
+                        neighboors.Sort();
+                        newImage.Set<byte>(i, j, (byte)neighboors[0]);
+                    }
+                }
+                Cv2.ImShow("Adaptive Filter (Min)", newImage);
+                AdaptiveButtons = false;
             }
         }
     }
 }
+
